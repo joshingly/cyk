d05732782d2d4adf93ac3e60cd5ff64c643d9c27
Joshua Antonishen <joshua@antonishen.com>
2012-03-10 20:29:16 -0500

    refactor can_generate

--- lib/cyk.rb
+++ lib/cyk.rb
@@ -6,10 +6,7 @@ class Cyk
   def self.can_generate string, grammar_file
     grammar_hash = Input.read_and_parse(grammar_file)
 
-    if CykAlgo.cyk_algo(string, grammar_hash)
-      return true
-    else
-      return false
-    end
+    return true if CykAlgo.cyk_algo(string, grammar_hash)
+    false
   end
 end
